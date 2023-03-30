5d8e9eb127fed64d61aef9d69ad45d86e4be646f
Raian Satyro <raiansatyro45@gmail.com>
2023-03-30 17:27:37 -0300

    Foi feito melhorias

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ const resultado = document.querySelector('#res');
 
 const table = document.createElement('table');
 table.classList.add('minha-tabela');
-
 const headerRow = document.createElement('tr');
 
 const nameHeader = document.createElement('th');
@@ -14,54 +13,94 @@ headerRow.appendChild(nameHeader);
 const enderecoHeader = document.createElement('th');
 enderecoHeader.textContent = 'Porcentagem';
 headerRow.appendChild(enderecoHeader);
+table.appendChild(headerRow);
 
 const totalHeader = document.createElement('th');
 totalHeader.textContent = 'total';
 headerRow.appendChild(totalHeader);
-
 table.appendChild(headerRow);
 
 const lojas = [
-  { nome: 'Bangu', porc: 4 , total: ''},
-  { nome: 'Barra', porc: 6 ,total: ''},
-  { nome: 'Cabo Frio', porc: 5 ,total: ''},
-  { nome: 'Campo Grande', porc: 6 ,total: ''},
+  { nome: 'Bangu', porc:4 , total: ''},
+  { nome: 'Barra', porc:6 ,total: ''},
+  { nome: 'Cabo Frio', porc:5 ,total: ''},
+  { nome: 'Campo Grande', porc:6 ,total: ''},
   { nome: 'Icarai', porc: 5 ,total: ''},
-  { nome: 'Macae', porc: 6 ,total: ''},
-  { nome: 'Nova Iguaçu rua', porc: 5 ,total: ''},
-  { nome: 'Nova Iguaçu shopping', porc: 7 ,total: ''},
-  { nome: 'Nilopolis', porc: 7, total: ''},
+  { nome: 'Macae', porc:  6 ,total: ''},
+  { nome: 'Nova Iguaçu rua', porc:5 ,total: ''},
+  { nome: 'Nova Iguaçu shopping', porc:7 ,total: ''},
+  { nome: 'Nilopolis', porc:  7, total: ''},
   { nome: 'Nova America', porc: 7, total: ''},
   { nome: 'Tijuca', porc: 7, total: ''},
   { nome: 'Vilar', porc: 5, total: ''},
-  { nome: 'Vinte e cinco', porc: 7, total: ''},
-  { nome: 'Caxias', porc: 9, total: ''},
-  { nome: 'Alcantara', porc: 5, total: ''},
-  { nome: 'Bangu Shopping', porc: 5, total: ''},
-  { nome: 'Madureira', porc: 4, total: ''},
+  { nome: 'Vinte e cinco', porc:7,total: ''},
+  { nome: 'Caxias', porc: 9,total: ''},
+  { nome: 'Alcantara', porc: 5,total: ''},
+  { nome: 'Bangu Shopping', porc: 5,total: ''},
+  { nome: 'Madureira', porc: 4,total: ''},
 ];
 
+for (let loja of lojas) {
+  const row = document.createElement('tr');
+  const nomeLoja = document.createElement('td');
+  nomeLoja.textContent = loja.nome;
+  row.appendChild(nomeLoja);
+
+  const enderecoLoja = document.createElement('td');
+  enderecoLoja.textContent = loja.porc;
+  row.appendChild(enderecoLoja);
+  table.appendChild(row);
+  
+  const total = document.createElement('td');
+  loja.total = valorDigitado.value * (loja.porc / 100);
+  total.textContent = loja.total.toFixed(0);
+  row.appendChild(total);
+  table.appendChild(row);
+}
+
 btnConfirma.addEventListener('click', () => {
-  resultado.innerHTML = '';
-
-  for (let loja of lojas) {
-    const row = document.createElement('tr');
-
-    const nomeLoja = document.createElement('td');
-    nomeLoja.textContent = loja.nome;
-    row.appendChild(nomeLoja);
-
-    const enderecoLoja = document.createElement('td');
-    enderecoLoja.textContent = loja.porc;
-    row.appendChild(enderecoLoja);
-
-    const total = document.createElement('td');
-    loja.total =  valorDigitado.value * (loja.porc/100);
-    total.textContent = loja.total.toFixed(0);
-    row.appendChild(total);
-
-    table.appendChild(row);
-  }
-
-  resultado.appendChild(table);
-});
+    // Limpa o resultado antes de gerar a tabela
+    resultado.innerHTML = '';
+  
+    const table = document.createElement('table');
+    table.classList.add('minha-tabela');
+  
+    const headerRow = document.createElement('tr');
+  
+    const nameHeader = document.createElement('th');
+    nameHeader.textContent = 'Nome da Loja';
+    headerRow.appendChild(nameHeader);
+  
+    const enderecoHeader = document.createElement('th');
+    enderecoHeader.textContent = 'Porcentagem';
+    headerRow.appendChild(enderecoHeader);
+    enderecoHeader.classList.add('centralizar'); // adiciona a classe para centralizar a coluna
+  
+    const totalHeader = document.createElement('th');
+    totalHeader.textContent = 'Total';
+    headerRow.appendChild(totalHeader);
+    table.appendChild(headerRow);
+  
+    for (let loja of lojas) {
+      const row = document.createElement('tr');
+      const nomeLoja = document.createElement('td');
+      nomeLoja.textContent = loja.nome;
+      row.appendChild(nomeLoja);
+  
+      const enderecoLoja = document.createElement('td');
+      enderecoLoja.textContent = loja.porc;
+      row.appendChild(enderecoLoja);
+      enderecoLoja.classList.add('centralizar'); // adiciona a classe para centralizar a coluna
+  
+      const total = document.createElement('td');
+      loja.total = Math.ceil(valorDigitado.value * (loja.porc/100));
+      total.textContent = loja.total.toFixed(0);
+      row.appendChild(total);
+      table.appendChild(row);
+    }
+  
+    resultado.appendChild(table);
+  });
+  
+  
+resultado.appendChild(table);
